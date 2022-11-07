d2d9fe59ef91f46f5ed0a220e2f764ee51273352
Tim Chen <timchen@bu.edu>
2022-11-06 19:36:43 -0500

    Fix downloader

--- Model/Data/coco_download.py
+++ Model/Data/coco_download.py
@@ -7,10 +7,7 @@ import zipfile
 class DataDownloader:
 
     def __init__(self) -> None:
-        if not os.path.exists('./Data/COCO'):
-            os.makedirs('./Data/COCO/train')
-            os.makedirs('./Data/COCO/val')
-            os.makedirs('./Data/COCO/test')
+        self.data_folder = './Data/COCO'
 
     def download_all(self) -> None:
         self.download_train()
@@ -19,23 +16,28 @@ class DataDownloader:
 
     
     def download_train(self) -> None:
+        train_folder = os.path.join(self.data_folder + 'train')
         train_data_url = 'http://images.cocodataset.org/zips/train2014.zip'
-        train_data_path = './Data/COCO/train2014.zip'
+        train_data_path = os.path.join(train_folder, 'train2014.zip')
         wget.download(train_data_url, out=train_data_path)
+        print('training data downloaded at ' + train_data_path)
         with zipfile.ZipFile(train_data_path) as train_zip:
-            train_zip.extractall(path='./Data/COCO/train')
+            train_zip.extractall(path=train_folder)
+            print('Training data extracted to' + train_folder)
 
     def download_val(self) -> None:
+        val_folder = os.path.join(self.data_folder + 'val')
         val_data_url = 'http://images.cocodataset.org/zips/val2014.zip'
-        val_data_path = './Data/COCO/val2014.zip'
+        val_data_path = os.path.join(val_folder, 'val2014.zip')
         wget.download(val_data_url, out=val_data_path)
         with zipfile.ZipFile(val_data_path) as val_zip:
-            val_zip.extractall(path='./Data/COCO/val')
+            val_zip.extractall(path=val_folder)
 
     
     def download_test(self) -> None:
+        test_folder = os.path.join(self.data_folder, 'test')
         test_data_url = 'http://images.cocodataset.org/zips/test2015.zip'
-        test_data_path = './Data/COCO/test2015.zip'
+        test_data_path = os.path.join(test_folder, 'test2015.zip')
         wget.download(test_data_url, test_data_path)
-        with zipfile.ZipFile(test_data_path)as test_zip:
-            test_zip.extractall(path='./Data/COCO/test')
+        with zipfile.ZipFile(test_data_path) as test_zip:
+            test_zip.extractall(path=test_folder)
